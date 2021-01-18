5a9d1207a81cd7c18ce45dab9361d9ab1ec6083a
abdelrahman-gad <abdelrahmangad95@gmail.com>
2021-01-18 18:06:07 +0200

    camilize

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('workspace is ready');
+// task 1 javascript.info -> translate border-left-width to translateBorderLeftWidth
+//  
+function camilize(str){
+// 1- split string to array 
+//     let wordsArr = str.split('-');
+// // 2- loop throgh every word [elem] and check the first elem id alphabet
+//     let modifiedWordsArr=wordsArr.map( word => {
+//         // test that first word
+//         return word[0].toUpperCase() + word.slice(1);       
+//     })
+//     console.log(modifiedWordsArr);
+
+  return str.split('-').map(word=>word[0].toUpperCase()+word.slice(1)).join('');
+
+
+
+}
+
+const result =  camilize('border-left-window');
+
+
+console.log(result);
